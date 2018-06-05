0d336137d9778c06d912548e5505f3f78be27755
h@di <hadi@hadisafari.ir>
2018-06-05 15:08:45 +0430

    add GET /rand and RandomNumberHandler

--- server/main.cpp
+++ server/main.cpp
@@ -1,7 +1,30 @@
 #include "server.hpp"
+#include <cstdlib> // for rand and srand
+#include <ctime>   // for time
 #include <iostream>
 
 using namespace std;
+
+class RandomNumberHandler : public RequestHandler {
+public:
+  Response *callback(Request *req) {
+    Response *res = new Response;
+    res->setHeader("Content-Type", "text/html");
+    string body;
+    body += "<!DOCTYPE html>";
+    body += "<html>";
+    body += "<body style=\"text-align: center;\">";
+    body += "<h1>AP HTTP</h1>";
+    body += "<p>";
+    body += "a random number in [1, 10] is: ";
+    body += to_string(rand() % 10 + 1);
+    body += "</p>";
+    body += "</body>";
+    body += "</html>";
+    res->setBody(body);
+    return res;
+  }
+};
 
 class LoginHandler : public RequestHandler {
 public:
@@ -13,11 +36,13 @@ public:
 };
 
 int main(int argc, char **argv) {
+  srand(time(NULL)); // for rand
   try {
     Server server(argc > 1 ? atoi(argv[1]) : 5000);
     server.get("/home_icon", new ShowImage("static/home.png"));
     server.get("/home", new ShowPage("static/home.html"));
     server.get("/login_page", new ShowPage("static/logincss.html"));
+    server.get("/rand", new RandomNumberHandler());
     server.post("/login", new LoginHandler());
     server.get("/", new ShowPage("static/home.html"));
     server.run();
