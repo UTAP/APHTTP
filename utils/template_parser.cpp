6ccd882d4be30bc04106cc61baad31c0fb58903c
Farzad Habibi <farzadhabibii98@gmail.com>
2019-05-09 22:09:02 +0430

    complete generating code in TemplateParser

--- utils/template_parser.cpp
+++ utils/template_parser.cpp
@@ -1,14 +1,18 @@
 #include "template_parser.hpp"
 using namespace std;
 
-TemplateParser::TemplateParser(string _filePath){
+TemplateParser::TemplateParser(string _filePath, Request *_req){
     filePath = _filePath;
     code = "";
+    req = _req;
+    variableCount = 0;
 }
 
 string TemplateParser::getParsedHtml(){
     string unparsedTemplate = readFile(filePath);
     parseTemplate(unparsedTemplate);
+    generateCode();
+    runGeneratedCode();
     return code;
 }
 
@@ -35,9 +39,11 @@ int TemplateParser::findEndOfCodeBlock(int startPosition, string &unparsedTempla
 }
 
 void TemplateParser::appendHTMLToCode(int begin, int end, string const &unparsedTemplate){
-    code += "cout << ";
-    code += unparsedTemplate.substr(begin, end - begin);
-    code += "\n";
+    code += "\nstring variable" + to_string(variableCount) + ";";
+    code += "\nvariable" + to_string(variableCount) + " = unparsedTemplate.substr(";
+    code += to_string(begin) + ", " + to_string(end - begin) + ");";
+    code += "\ncout << variable" + to_string(variableCount) + ";" ;
+    variableCount ++;
 }
 
 void TemplateParser::appendCodeBlockToCode(int begin, int end, string &unparsedTemplate){
@@ -46,4 +52,31 @@ void TemplateParser::appendCodeBlockToCode(int begin, int end, string &unparsedT
     //TODO: Add throwing exception for end not found.
     code += unparsedTemplate.substr(begin + beginCodeBlockTag.size(),
                                     end - begin - beginCodeBlockTag.size() - endCodeBlockTag.size());
+}
+
+void TemplateParser::generateCode(){
+    addReadFromTemplateToCode();
+    addIncludesToCode();
+    addReturnToCode();
+}
+
+void TemplateParser::runGeneratedCode(){
+
+}
+
+void TemplateParser::addIncludesToCode(){
+    string include = "#include <iostream>\n";
+    include += "#include <string>\n";
+    include += "#include \"utils/request.hpp\"\n";
+    include += "#include \"utils/utilities.hpp\"\n";
+    include += "using namespace std;\n";
+    code = include + "int main()\n{\n" + code + "\n";
+}
+
+void TemplateParser::addReadFromTemplateToCode(){
+    code = "string unparsedTemplate = readFile(\"" + filePath + "\");\n" + code;
+}
+
+void TemplateParser::addReturnToCode(){
+    code += "return 0;\n}\n";
 }
--- utils/template_parser.hpp
+++ utils/template_parser.hpp
@@ -1,7 +1,9 @@
 #ifndef __TEMPLATE_PARSER__
 #define __TEMPLATE_PARSER__
 #include <string>
+#include <iostream>
 #include "../utils/utilities.hpp"
+#include "../utils/request.hpp"
 const std::string beginCodeBlockTag =  "<%";
 const std::string endCodeBlockTag = "%>";
 
@@ -9,15 +11,22 @@ class TemplateParser {
 private:
     std::string filePath;
     std::string code;
+    Request *req;
+    int variableCount;
 
     void parseTemplate(std::string unparsedTemplate);
     int findBeginOfCodeBlock(int startPosition, std::string &unparsedTemplate);
     int findEndOfCodeBlock(int startPosition, std::string &unparsedTemplate);
     void appendHTMLToCode(int begin, int end, std::string const &html);
     void appendCodeBlockToCode(int begin, int end, std::string &unparsedTemplate);
+    void generateCode();
+    void addIncludesToCode();
+    void addReadFromTemplateToCode();
+    void addReturnToCode();
+    void runGeneratedCode();
 
 public:
-    TemplateParser(std::string _filePath);
+    TemplateParser(std::string _filePath, Request *_req);
     std::string getParsedHtml();
 };
 
